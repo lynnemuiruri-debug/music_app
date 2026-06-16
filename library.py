b2e531e0d74c8704ca9d3bab5f576fa19654426e
lynnemuiruri-debug <lynne.muiruri@strathmore.edu>
2026-06-17 00:55:48 +0300

    Add Song class and song library - Part 1

---
+++ library.py
@@ -0,0 +1,17 @@
+from song import Song
+
+Song1 = Song(1, "Espresso", "Sabrina Carpenter", "Pop", "2024", "")
+Song2 = Song(2, "Vampire", "Olivia Rodrigo", "Pop", "2023", "")
+Song3 = Song(3, "As It Was", "Harry Style", "Pop", "2022", "")
+Song4 = Song(4, "Chemical", "Post Malone", "Hip-hop", "2023", "")
+Song5 = Song(5, "Cruel World", "Kendrick Lamar", "Hip-hop", "2022", "")
+Song6 = Song(6, "Rich Flex", "Drake & 21Savage", "Hip-hop", "2022", "")
+Song7 = Song(7, "Calm Down", "rema & Selena Gomez", "Afrobeats", "2022", "")
+Song8 = Song(8, "Temperature", "Burna Boy", "Afrobeats", "2022", "")
+Song9 = Song(9, "Love Nwantiti", "Ckay", "Afrobeats", "2021", "")
+Song10 = Song(10, "Creepin", "Metro Boomin & The Weeknd ", "R&B", "2023", "")
+Song11 = Song(11, "Golden Hour", "JVKE", "R&B", "2022", "")
+Song12 = Song(12, "She Knows", "J.Cole", "R&B", "2023", "")
+songs = [Song1, Song2, Song3, Song4, Song5, Song6, Song7, Song8, Song9, Song10, Song11, Song12]
+def get_all_songs():
+    return songs
---
+++ song.py
@@ -0,0 +1,8 @@
+class Song:
+    def __init__(self, id,title, artist, genre,year, file_path):
+        self.id = id
+        self.title = title
+        self.artist = artist
+        self.genre = genre
+        self.year = year
+        self.file_path = file_path
